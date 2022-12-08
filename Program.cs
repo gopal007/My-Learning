d2e90680926abd769ec8aae7f547cc509fe15c25
Gopal Gawande <gopalgawande94@example.com>
2022-12-08 17:22:58 +0530

    Added Bubble sort

--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@
 int []arr = {7,1,5,3,2,4,9,8,88,57};
 Console.WriteLine("Initial array : " + String.Join(",", arr));
         
-int []arrr = Sort.SelectionSort(arr, arr.Length);
-Console.WriteLine("Selection Sort : " + String.Join(",", arrr));
+//int []arrr = Sort.SelectionSort(arr, arr.Length);
+//Console.WriteLine("Selection Sort : " + String.Join(",", arrr));
 
+
+int []arrr = Sort.BubbleSort(arr);
+Console.WriteLine("Bubble Sort : " + String.Join(",", arrr));
--- SortingAlgo/Sort.cs
+++ SortingAlgo/Sort.cs
@@ -5,6 +5,7 @@ namespace Sorting {
     public static class Sort
     {
         public static int[] SelectionSort(int [] arr, int n){
+            Console.WriteLine("Selection Sort -- > Time complexity is n square");
             for(int i=0; i<n;i++){
                 int indexmin = i;
                 for(int j=i+1; j<n; j++){
@@ -18,6 +19,22 @@ namespace Sorting {
                 //Console.WriteLine(String.Join(",", arr));
             }
             
+            return arr;
+        }
+
+        public static int[] BubbleSort(int [] arr){
+            Console.WriteLine("Bubble Sort -- > Time complexity is n square");
+
+            for(int i=0; i<arr.Length; i++){
+                for(int j = 0; j < arr.Length-i-1; j++){
+                    if(arr[j+1] < arr[j]){
+                        int temp = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j+1] = temp;
+                    }
+                }
+                //Console.WriteLine(i+1 + " : " + String.Join(",", arr));
+            }
             return arr;
         }
     }
